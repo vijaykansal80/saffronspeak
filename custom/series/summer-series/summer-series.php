367bd6f9b71999cb1891e1f26884262108a2c673
sarah ✈ semark <sarah@triggersandsparks.com>
2014-06-24 22:24:44 -0400

    Add 'read more' link to summer series preview on homepage.

--- custom/series/summer-series/summer-series.php
+++ custom/series/summer-series/summer-series.php
@@ -3,8 +3,10 @@
 
     <div class="panel clearfix">
         <figure>
-            <img class="circular" alt="Paisley Fraiche Duvet Cover" src="<?php echo $path; ?>/paisley-fraiche.jpg" width="362" height="362" /></a><figcaption>Paisley Fraiche Duvet Cover and Shams</figcaption><br />
+            <a href="http://www.saffronmarigold.com/blog/saffron-marigolds-summer-series-ideas-to-redecorate-for-summer/"><img class="circular" alt="Paisley Fraiche Duvet Cover" src="<?php echo $path; ?>/paisley-fraiche.jpg" width="362" height="362" /></a>
+            <figcaption>Paisley Fraiche Duvet Cover and Shams</figcaption>
         </figure>
        <p>We&#8217;re here to help you enjoy this beloved season in all its glory&mdash;open the windows, let the sunshine in, change things around and embrace color. Use Saffron Marigold linens to dazzle and cheer up your <a href="http://www.saffronmarigold.com/blog/saffron-marigolds-summer-series-ideas-to-redecorate-for-summer/#bedrooms" >bedrooms</a>, <a href="http://www.saffronmarigold.com/blog/saffron-marigolds-summer-series-ideas-to-redecorate-for-summer/#dining-rooms">dining rooms</a>, <a href="http://www.saffronmarigold.com/blog/saffron-marigolds-summer-series-ideas-to-redecorate-for-summer/#living">living spaces</a> and <a href="http://www.saffronmarigold.com/blog/saffron-marigolds-summer-series-ideas-to-redecorate-for-summer/#bath">baths</a>!</p>
+       <p class="read-more"><a href="http://www.saffronmarigold.com/blog/saffron-marigolds-summer-series-ideas-to-redecorate-for-summer/">Read more</a></p>
     </div>
 </div>
