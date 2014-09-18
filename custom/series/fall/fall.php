bd79c2558626ded94209d82d41609eea598312fc
sarah semark <sarah@triggersandsparks.com>
2014-09-18 19:09:27 -0400

    Update blog homepage to use new intro and TOC.

--- custom/series/fall/fall.php
+++ custom/series/fall/fall.php
@@ -1,11 +1,7 @@
-<p>Welcome to our Fall Series posts. Here you will find wonderful ideas to use all around your home! Do let us know what you think of these ideas and, of course, if you have any questions, <a href="http://www.saffronmarigold.com/catalog/contact_us.php">simply send us an email</a>.</p>
+<p>Across much of the world, Fall is the most beautiful time of the year. From gold and amber to flaming reds and scarlet, the colors outdoors are truly a feast for the eyes. In this Fall series, we bring you season inspired decorating ideas to introduce some of this autumnal beauty into your home, and blur the lines between the outside and the inside.</p>
 
-    <div class="panel clearfix">
-            <figure>
-                <img class="circular" alt="Fall series" src="<?php echo $path; ?>/orange-blossom.jpg" width="362" height="362" /></a>
-                <figcaption>Our new Orange Blossom print</figcaption>
-            </figure>
-        <p>View our ideas for your: <a href="/blog/room-by-room-fall-decorating-ideas#Bedroom">Bedroom</a> &bull; <a href="/blog/room-by-room-fall-decorating-ideas#Living">Living Room</a> &bull; <a href="/blog/room-by-room-fall-decorating-ideas#Dining">Dining Room</a> &bull; <a href="/blog/room-by-room-fall-decorating-ideas#Outdoor">Outdoor Dining</a> &bull; <a href="/blog/room-by-room-fall-decorating-ideas#Bath">Bath</a></p>
-		<p><a href="http://www.saffronmarigold.com/catalog/shop_by_print.php?cPath=59">Click here to see our best selling prints</a> this season. Top sellers are listed first and updated weekly. <a href="http://www.saffronmarigold.com/catalog/directory.php?cPath=55">Click here to see our top 10 best selling products</a>&mdash;also updated weekly.</p>
-        <p class="read-more"><a href="/blog/room-by-room-fall-decorating-ideas/">Read our Fall Series</a></p>
-    </div>
+<p class="read-more"><a href="/blog/room-by-room-fall-decorating-ideas/">Read our Fall Series</a></p>
+
+<div class="table-of-contents">
+	<a href="/blog/room-by-room-fall-decorating-ideas#Bedroom" class="bedroom">Bedroom</a><a href="/blog/room-by-room-fall-decorating-ideas#Living" class="living-room">Living Room</a><a href="/blog/room-by-room-fall-decorating-ideas#Dining" class="dining-room">Dining Room</a><a href="/blog/room-by-room-fall-decorating-ideas#Outdoor" class="outdoor-dining">Outdoor Dining</a><a href="/blog/room-by-room-fall-decorating-ideas#Bath" class="bathroom">Bath</a></p>
+</div>
