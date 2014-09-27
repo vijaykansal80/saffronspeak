183b43518b72d0585acee250924a9d28732da266
sarah semark <sarah@triggersandsparks.com>
2014-09-27 19:24:41 -0400

    Add new and updated badges.

--- custom/custom_functions.php
+++ custom/custom_functions.php
@@ -577,8 +577,12 @@ class archive_looper extends thesis_custom_loop {
                 ?>
                 <div class="subcategory <?php echo smarter_slug($subcategory); ?>">
                     <h2><?php echo $subcategory->name; ?></h2>
-                    <?php if ($subcategory->name == "Fall Series"): ?>
-                        <img class="new-badge" src="<?php bloginfo(stylesheet_directory); ?>/custom/images/new-for-2014.png" alt="New for 2014"/>
+                    <?php if ($subcategory->name == "Fall"): ?>
+                        <img class="badge" src="<?php bloginfo(stylesheet_directory); ?>/custom/images/new-for-2014.png" alt="New for 2014"/>
+                    <?php elseif ($subcategory->name == "Thanksgiving"): ?>
+                        <img class="badge" src="<?php bloginfo(stylesheet_directory); ?>/custom/images/new.png" alt="New"/>
+                    <?php elseif ($subcategory->name == "Holidays"): ?>
+                        <img class="badge" src="<?php bloginfo(stylesheet_directory); ?>/custom/images/updated.png" alt="Updated"/>
                     <?php endif; ?>
                     <p class="read-more"><a href="<?php echo get_category_link($subcategory->term_id); ?>">Read more</a></p>
                     <a class="div-link" href="<?php echo get_category_link($subcategory->term_id); ?>"></a>
