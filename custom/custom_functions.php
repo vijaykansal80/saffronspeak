8367e7cb3c160b53de078bc6a9e72f9b0e001c09
sarah semark <sarah@triggersandsparks.com>
2014-10-28 06:56:30 +0530

    Tweak panel styles and change series panel around.

--- custom/custom_functions.php
+++ custom/custom_functions.php
@@ -673,23 +673,25 @@ function series_navigation() {
         if ($category->category_parent === 5): ?>
         <section class="panel series-navigation">
 
-            <h3>Explore more of our <a href="<?php echo $series_link; ?>"><?php echo $category->name; ?></a> series</h3>
             <p><?php echo $category->description; ?></p>
-            <?php $previous_post = get_adjacent_post(true, '', true); ?>
-            <?php if (!empty($previous_post)): ?>
-                <div class="previous-post">
+            
+            <div class="previous-post">
+                <?php $previous_post = get_adjacent_post(true, '', true); ?>
+                <?php if (!empty($previous_post)): ?>                
                     <span>&laquo; Previous post in series</span>
                     <a href="<?php echo get_permalink($previous_post->ID); ?>"><?php echo $previous_post->post_title; ?></a>
-                </div>
-            <?php endif; ?>
+                <?php endif; ?>
+            </div>
 
-            <?php $next_post = get_adjacent_post(true, '', false); ?>
-            <?php if (!empty($next_post)): ?>
-                <div class="next-post">
+            <div class="next-post">
+                <?php $next_post = get_adjacent_post(true, '', false); ?>
+                <?php if (!empty($next_post)): ?>
                     <span>Next post in series &raquo;</span>
                     <a href="<?php echo get_permalink($next_post->ID); ?>"><?php echo $next_post->post_title; ?></a>
-                </div>
-            <?php endif; ?>
+                <?php endif; ?>
+            </div>
+
+            <p class="read-more"><a href="<?php echo $series_link; ?>">View all <?php echo $category->name; ?> posts</a></p>
         </section>
         <?php endif;
 
