04920fa5e7e1640bdf25bffb886757c929a3dd6b
sarah semark <sarah@triggersandsparks.com>
2014-10-28 12:16:24 +0530

    Show simplified navigation in series navigation block.

--- custom/custom_functions.php
+++ custom/custom_functions.php
@@ -678,20 +678,21 @@ function series_navigation() {
             <div class="previous-post">
                 <?php $previous_post = get_adjacent_post(true, '', true); ?>
                 <?php if (!empty($previous_post)): ?>                
-                    <span>&laquo; Previous post in series</span>
-                    <a href="<?php echo get_permalink($previous_post->ID); ?>"><?php echo $previous_post->post_title; ?></a>
+                    <a href="<?php echo get_permalink($previous_post->ID); ?>">&laquo; Previous post</a>
                 <?php endif; ?>
+            </div>
+
+            <div class="all-posts">
+                <a href="<?php echo $series_link; ?>">View all <?php echo $category->name; ?> posts</a>
             </div>
 
             <div class="next-post">
                 <?php $next_post = get_adjacent_post(true, '', false); ?>
                 <?php if (!empty($next_post)): ?>
-                    <span>Next post in series &raquo;</span>
-                    <a href="<?php echo get_permalink($next_post->ID); ?>"><?php echo $next_post->post_title; ?></a>
+                    <a href="<?php echo get_permalink($next_post->ID); ?>">Next post &raquo;</a>
                 <?php endif; ?>
             </div>
 
-            <p class="read-more"><a href="<?php echo $series_link; ?>">View all <?php echo $category->name; ?> posts</a></p>
         </section>
         <?php endif;
 
