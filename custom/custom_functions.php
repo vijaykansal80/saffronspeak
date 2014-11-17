ee79d808ea1f454bf2e4ba4cfb74c774189fcb0d
sarah semark <sarah@triggersandsparks.com>
2014-11-17 18:20:32 +0545

    Nix WordPress captions and use proper HTML5 captions instead.

--- custom/custom_functions.php
+++ custom/custom_functions.php
@@ -112,6 +112,52 @@ function new_excerpt_more($more) {
 add_filter('excerpt_more', 'new_excerpt_more');
 
 
+/* Replace WordPress captions with HTML5 captions */
+
+function cleaner_caption( $output, $attr, $content ) {
+
+    /* We're not worried abut captions in feeds, so just return the output here. */
+    if ( is_feed() )
+        return $output;
+
+    /* Set up the default arguments. */
+    $defaults = array(
+        'id' => '',
+        'align' => 'alignnone',
+        'width' => '',
+        'caption' => ''
+    );
+
+    /* Merge the defaults with user input. */
+    $attr = shortcode_atts( $defaults, $attr );
+
+    /* If the width is less than 1 or there is no caption, return the content wrapped between the [caption]< tags. */
+    if ( 1 > $attr['width'] || empty( $attr['caption'] ) )
+        return $content;
+
+    /* Set up the attributes for the caption <div>. */
+    $attributes = ( !empty( $attr['id'] ) ? ' id="' . esc_attr( $attr['id'] ) . '"' : '' );
+    $attributes .= ' class="' . esc_attr( $attr['align'] ) . '"';
+
+    /* Open the caption <div>. */
+    $output = '<figure' . $attributes .'>';
+
+    /* Allow shortcodes for the content the caption was created for. */
+    $output .= do_shortcode( $content );
+
+    /* Append the caption text. */
+    $output .= '<figcaption>' . $attr['caption'] . '</figcaption>';
+
+    /* Close the caption </div>. */
+    $output .= '</figure>';
+
+    /* Return the formatted, clean caption. */
+    return $output;
+}
+
+add_filter( 'img_caption_shortcode', 'cleaner_caption', 10, 3);
+add_filter( 'thesis_img_caption_shortcode', 'cleaner_caption', 10, 3);
+
 
 
 
