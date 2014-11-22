f657f861ae27fc331d48c88cd2f1799ad8edc112
sarah semark <sarah@triggersandsparks.com>
2014-11-22 19:04:22 +0530

    Change text of 'read more' buttons when expanded/collapsed.

--- custom/scripts.js
+++ custom/scripts.js
@@ -3,11 +3,15 @@ jQuery(document).ready(function(){
       e.preventDefault();
       jQuery('.subcategory-expander div').slideToggle();
       jQuery(this).find('i').toggleClass('icon-caret-up');
+      jQuery(this).find('.main').toggle();
+      jQuery(this).find('.extra').toggle();
     });
 
     jQuery('a.parent-expander-link').click(function(e) {
       e.preventDefault();
       jQuery(this).parent().prev('section.expander').slideToggle(800);
       jQuery(this).find('i').toggleClass('icon-caret-up');
+      jQuery(this).find('.main').toggle();
+      jQuery(this).find('.extra').toggle();
     });
 });
