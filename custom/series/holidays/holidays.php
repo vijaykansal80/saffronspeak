bcd638de8caea6cf20c12b67d658ad6059434bd0
sarah semark <sarah@triggersandsparks.com>
2014-11-24 17:36:47 +0530

    Style main blog page block for holiday feature.

--- custom/series/holidays/holidays.php
+++ custom/series/holidays/holidays.php
@@ -1,8 +1,7 @@
 <div class="panel clearfix">
   <figure>
-    <img class="circular" alt="Thanksgiving Table" src="<?php echo $path; ?>/table-setting.jpg" width="362" height="362" /></a>
-    <figcaption>Get ready for holiday gifting!</figcaption>
+    <img class="circular" alt="Thanksgiving Table" src="<?php echo $path; ?>/ruby-kilim.jpg" width="333" height="333" /></a>
   </figure>
-  <p>We have put together a Decor and Gift guide this holiday season to help you with your shopping–for yourself as well as for others!</p>
+  <p>Searching for the perfect holiday gift? We&rsquo;ve put together a Decor and Gift guide this holiday season to help with your shopping–for yourself as well as for others!</p>
   <p class="read-more"><a href="/blog/christmas-gift-guide-day-bedding-linen-ideas/">Read our Holiday Series</a></p>
 </div>
