b339e0269cf744f6b605daf7c7bc984f6db1d6cb
sarah semark <sarah@triggersandsparks.com>
2014-12-08 21:40:58 +0530

    Add code and basic styling for main promotional block.

---
+++ custom/promotions/holiday-sale.php
@@ -0,0 +1,154 @@
+<div class="panel">
+  <p>Welcome to our holiday sale. For this year&rsquo;s holiday sale, we have decided to organize our prints based on color-ways. This will be a four-part series. First up are our prints that have whites and blacks predominately in them.</p>
+</div>
+              
+
+
+
+
+
+<h2><img src="<?php echo $path; ?>/images/10.gif" alt="10% off these prints" /></h2>                         
+                
+<section class="threecol">
+  <div class="col">
+   <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=V_GLACE&amp;products_id=663"><img alt="Vanilla Glacé" src="<?php echo $path; ?>images/swatches/vanilla-glace.jpg" class="swatch" /></a>
+   <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=V_GLACE&amp;products_id=663"><img src="<?php echo $path; ?>images/text/vanilla-glace.gif" alt="Vanilla Glacé" /></a>
+  </div>
+
+  <div class="col">
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=R_MANSOUR&amp;products_id=988"><img alt="Royal Mansour"  src="<?php echo $path; ?>images/swatches/royal-mansour.jpg" class="swatch" /></a>
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=R_MANSOUR&amp;products_id=988"><img src="<?php echo $path; ?>images/text/royal-mansour.gif" alt="Royal Mansour" width="177" height="44" /></a>
+  </div>
+
+  <div class="col"> 
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=I_LACE&amp;products_id=524"><img alt="Ivy Lace" src="<?php echo $path; ?>images/swatches/ivy-lace.jpg" class="swatch" /></a>
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=I_LACE&amp;products_id=524"><img src="<?php echo $path; ?>images/text/ivy-lace.gif" alt="Ivy Lace" width="177" height="44" /></a>
+  </div>
+</section>
+
+<section class="threecol">
+  <div class="col">
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=D_PEACOCK&amp;products_id=583"><img alt="Dance o Peacock"  src="<?php echo $path; ?>images/swatches/dance-o-peacock.jpg" class="swatch" /></a>
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=D_PEACOCK&amp;products_id=583"><img src="<?php echo $path; ?>images/text/dance-o-peacock.gif" alt="Dance o Peacock" width="177" height="44" /></a>
+  </div>
+
+  <div class="col">
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=B_VEIL&amp;products_id=890"><img alt="Bridal Veil"  src="<?php echo $path; ?>images/swatches/bridal-veil.jpg" class="swatch" /></a>
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=B_VEIL&amp;products_id=890"><img src="<?php echo $path; ?>images/text/bridal-veil.gif" alt="Bridal Veil" width="177" height="44" /></a>
+  </div>
+</section>
+
+<h2><img src="<?php echo $path; ?>images/15.gif" alt="15% off these prints" /></h2>
+
+<section class="threecol">
+  <div class="col">
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=FILI&amp;products_id=803"><img alt="Filigree"  src="<?php echo $path; ?>images/swatches/filigree.jpg" class="swatch" /></a>
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=FILI&amp;products_id=803"><img src="<?php echo $path; ?>images/text/filigree.gif" alt="Filigree" width="177" height="44" /></a>
+  </div>
+
+  <div class="col">
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=L_DREAMS&amp;products_id=536"><img alt="Lavender Dreams"  src="<?php echo $path; ?>images/swatches/lavender-dreams.jpg" class="swatch" /></a>
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=L_DREAMS&amp;products_id=536"><img src="<?php echo $path; ?>images/text/lavender-dreams.gif" alt="Lavender Dreams" width="177" height="44" /></a>
+  </div>
+
+  <div class="col">
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=CASA&amp;products_id=949"><img alt="Casablanca" src="<?php echo $path; ?>images/swatches/casablanca.jpg" class="swatch" /></a>
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=CASA&amp;products_id=949"><img src="<?php echo $path; ?>images/text/casablanca.gif" alt="Casablanca" width="177" height="44" /></a>
+  </div>
+
+  <div class="col">
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=K_NOIR&amp;products_id=535"><img alt="Kilim Noir" src="<?php echo $path; ?>images/swatches/kilim-noir.jpg" class="swatch" /></a>
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=K_NOIR&amp;products_id=535"><img src="<?php echo $path; ?>images/text/kilim-noir.gif" alt="Kilim Noir" width="177" height="44" /></a>
+  </div>
+</section>
+
+
+<h2><img src="<?php echo $path; ?>images/20.gif" alt="20% off these prints" /></h2>
+
+<section class="threecol">
+  <div class="col">
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=P_LAIT&amp;products_id=558"><img alt="Paisley Au Lait"  src="<?php echo $path; ?>images/swatches/paisley-au-lait.jpg" class="swatch" /></a>
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=P_LAIT&amp;products_id=558"><img src="<?php echo $path; ?>images/text/paisley-au-lait.gif" alt="Paisley Au Lait" width="177" height="44" /></a>
+  </div>
+
+  <div class="col">
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=L_PAPILLONS&amp;products_id=602"><img alt="Chasing Butterflies" src="<?php echo $path; ?>images/swatches/chasing-butterflies.jpg" class="swatch" /></a>
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=L_PAPILLONS&amp;products_id=602"><img src="<?php echo $path; ?>images/text/chasing-butterflies.gif" alt="Chasing Butterflies" width="177" height="44" /></a>
+  </div>
+
+  <div class="col"> 
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=D_INDIA_NP&amp;products_id=777"><img alt="The Nizam's Pearls"  src="<?php echo $path; ?>images/swatches/the-nizams-pearls.jpg" class="swatch" /></a>
+    <a href="http://www.saffronmarigold.com/catalog/collection_page.php?cPath=59&amp;model=D_INDIA_NP&amp;products_id=777"><img src="<?php echo $path; ?>images/text/the-nizams-pearls.gif" alt="The Nizam's Pearls" width="177" height="44" /></a>
+  </div>
+</section>
+
+
+<h2>Holiday Special: Save 50% off shipping</h2>
+
+<section class="twocol">
+  <div class="col">
+  <h2>Holiday Special: Save 50% off shipping</h2>
+  US Orders: No order minimum*<br>
+  Non-US Orders: orders over $150*<br>
+  *Valid for orders up to $1000
+
+      <h3>Note</h3> 
+    <ul>
+      <li>Qualifying order total excludes shipping charges.</li>
+      <li>Promotion applicable only to in stock items.</li>
+      <li>Applicable shipping discount will be automatically applied when you check out.</li> 
+      <li>No discount code required. Only 1 discount can be applied per order.</li>
+      <li>Promotion does not apply to orders over $1000 which will incur regular shipping charges.</li>
+    </ul>
+  </div>
+
+  <div class="col">
+  <h3>Shipping cost for US orders</h3>
+    <table class="shipping-chart">
+      <tr>
+        <td>$0-$30</td>
+        <td>$6.99</td>
+        <td>$3.49</td>
+      </tr>
+      <tr>
+        <td>$30-$50</td>
+        <td>$9.99</td>
+        <td>$4.99</td>
+      </tr>
+      <tr>
+        <td>$50-$100</td>
+        <td>$12.99</td>
+        <td>$6.99</td>
+      </tr>
+      <tr>
+        <td>$100-$150</td>
+        <td>$14.99</td>
+        <td>$7.99</td>
+      </tr>
+      <tr>
+        <td>$150-$200</td>
+        <td>$19.00</td>
+        <td>$9.99</td>
+      </tr>
+      <tr>
+        <td>$200-$250</td>
+        <td>$24.99</td>
+        <td>$12.99</td>
+      </tr>
+      <tr>
+        <td>$250-$300</td>
+        <td>$29.99</td>
+        <td>$14.99</td>
+      </tr>
+      <tr>
+        <td>$300+</td>
+        <td>10%</td>
+        <td>5%</td>
+      </tr>
+    </table>
+  *Non-US orders: we will contact you with the discounted shipping cost.
+  </div>
+</section>
+
+
+
