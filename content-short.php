d0103c5b7d9922f6b4c909a39046372aaaeb930d
sarah <sarah@triggersandsparks.com>
2015-04-04 02:30:51 +0300

    Ensure that search terms are highlighted; format no-results page.

--- content-short.php
+++ content-short.php
@@ -7,17 +7,19 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'short-excerpt' ); ?>>
 
 <?php // Show featured images
-if (has_post_thumbnail()): ?>
+if ( has_post_thumbnail() ): ?>
   <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
-    <?php the_post_thumbnail(''); ?>
+    <?php the_post_thumbnail(); ?>
   </a>
 <?php endif; ?>
 
   <div class="entry-content">
     <?php the_title( sprintf( '<h4 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h4>' ); ?>
     <?php
-    if ( "search" === $archive ):
+    // For Relevanssi's search term highlighting to work, we need to use the_excerpt
+    if ( is_search() ):
       the_excerpt();
+    // Otherwise, we'll use a custom excerpt that allows us greater control
     else:
       the_advanced_excerpt( 'length=40&use_words=1&no_custom=1&ellipsis=&finish_sentence=1' );
     endif;
