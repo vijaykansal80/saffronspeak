02fec7b7dfad75ef2c8275cbd4c79ba495389c4b
sarah <sarah@triggersandsparks.com>
2015-04-03 23:21:50 +0300

    Don't show the post header for parent posts.

--- content-single.php
+++ content-single.php
@@ -1,16 +1,22 @@
 <?php
 /**
- * @package safflower
+ * @package Safflower
  */
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<div class="entry-meta">
-			<?php safflower_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+
+	<?php
+	// If this is the parent (sticky) post, we won't show the post header
+	if ( ! is_sticky() ):
+	?>
+		<header class="entry-header">
+			<div class="entry-meta">
+				<?php safflower_posted_on(); ?>
+			</div><!-- .entry-meta -->
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</header><!-- .entry-header -->
+	<?php endif; ?>
 
 	<div class="entry-content">
 		<?php the_content(); ?>
