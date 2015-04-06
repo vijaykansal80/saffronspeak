d1b963172082921e08268bc3d906b9141bfb4045
sarah <sarah@triggersandsparks.com>
2015-04-07 01:59:53 +0300

    Style Jetpack carousel a bit, and ensure that comments can't be added.

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -42,3 +42,15 @@ function safflower_jetpack_setup() {
 
 }
 add_action( 'after_setup_theme', 'safflower_jetpack_setup' );
+
+/**
+* Remove ability to comment on Jetpack carousel images
+*/
+function filter_media_comment_status( $open, $post_id ) {
+    $post = get_post( $post_id );
+    if( $post->post_type == 'attachment' ) {
+        return false;
+    }
+    return $open;
+}
+add_filter( 'comments_open', 'filter_media_comment_status', 10 , 2 );
