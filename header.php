6a6eb02d5b7e844ba4f78f286279b1f7c99b2144
sarah <sarah@triggersandsparks.com>
2015-04-09 18:17:43 +0300

    Rework mobile menu and tweak overall menu code so it's mobile-first. Still requires some tweaking & fine-tuning.

--- header.php
+++ header.php
@@ -51,7 +51,12 @@
     </div><!-- .site-contact -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e( 'Primary Menu', 'safflower' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="screen-reader-text"><?php _e( 'Primary Menu', 'safflower' ); ?></span>
+      </button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
