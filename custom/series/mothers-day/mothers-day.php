1c6eec4ab09d9556ab86084fc7f223a283a80978
Vladimir Stamatovic <stamat@mrgud>
2015-05-05 14:41:13 +0200

    Changed images fro mothers day. Added read more link. Removed paragraph above mothers day block.

--- custom/series/mothers-day/mothers-day.php
+++ custom/series/mothers-day/mothers-day.php
@@ -1,7 +1,7 @@
-<p>Just in time for Mother&#8217;s Day, we have put together some inspiring ways to tell Mom &#8220;thanks for everything&#8221; and for expressing love and gratitude to her.  We’ll focus on high quality, hand made items, do-it-yourself projects and special recipes. These reflect our commitment to the personal touch&#8211;in our handcrafted linens, our genuine dedication to customer service and our belief that <strong>you</strong> find joy in what comes from heart and hand.</p>
 <div class="panel clearfix">
   <figure>
       <img class="borderless" alt="Show Mom you care" src="<?php echo $path; ?>/motherchild.png" width="362" height="362" /></a>
   </figure>
   <p>We first start with <a href="<?php echo get_category_link($category->term_id); ?>#pamper">3 ways you can pamper mom</a>&#8211;sleeping on beautiful linens and breakfast in bed; a Sunday morning brunch and/or a bathroom makeover. We have gift ideas for <a href="<?php echo get_category_link($category->term_id); ?>#mom-by-type">your traveller mom, bohemian mom and sentimental mom</a>. Finally we have put together a few ideas for mom who <a href="<?php echo get_category_link($category->term_id); ?>#mom-by-color">loves color and for mom who prefers the pure simplicity of white!</a></p>
+  <span class="readMore"><a href="<?php echo home_url() ?>/ideas-for-mothers-day/">Read More</a></span>
 </div>
